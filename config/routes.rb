1b2f30b4f26f5411c96ad34c2da708123e85c4e3
KylIiIe <sofiiahaponova00@gmail.com>
2023-05-28 10:52:39 +0000

    Add devise for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'page/home'
+  devise_for :users, controllers: { registrations: 'users/registrations', passwords: 'users/passwords', sessions: 'users/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "page#home"
+  get '/home', to: 'page#home'
 end
